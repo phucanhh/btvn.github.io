1b8165a54a1effb63e66752b3262d3c79ecf7721
phucanhh <118phucanh@gmail.com>
2022-10-29 21:35:23 +0700

    hjhj

---
+++ index.js
@@ -0,0 +1,23 @@
+var chu1={
+    id:1,
+    name:"a",
+}
+var chu2={
+    id:2,
+    name:"b",
+}
+var chu3={
+    id:3,
+    name:"c",
+}
+var chu4={
+    id:4,
+    name:"d",
+}
+
+var chucais=[chu1,chu2,chu3,chu4]
+
+for (let i=0; i<10; i++) {
+   
+}
+console.log(chucais);
